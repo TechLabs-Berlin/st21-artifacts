c6a0af4b2d8dbd61cb9dd96eb4c75f4128716388
sernawills <luciana.sernawills@student.hpi.uni-potsdam.de>
2021-06-20 17:02:26 +0200

    added links to landing page menu, fixed layout
    :)

--- WD/src/scripts/components/LandingPage.js
+++ WD/src/scripts/components/LandingPage.js
@@ -1,34 +1,47 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const LandingPage = () => (
   <div className="landing-page">
     <div className="container-actions">
       <div className="action">
         <div className="action-content">
-          <a>
+          <NavLink
+            to="/search"
+            activeClassName="is-active"
+          >
             find an <span className="boldItalic">Artifact</span>
-          </a>
+          </NavLink>
         </div>
       </div>
       <div className="action">
         <div className="action-content">
-          <a>
+          <NavLink
+            to="/"
+            activeClassName="is-active"
+          >
             find a <span className="boldItalic">service / collaborator</span>
-          </a>
+          </NavLink>
         </div>
       </div>
       <div className="action">
         <div className="action-content">
-          <a>
+          <NavLink
+            to="/offerArtifact"
+            activeClassName="is-active"
+          >
             offer an <span className="boldItalic">Artifact</span>
-          </a>
+          </NavLink>
         </div>
       </div>
       <div className="action">
         <div className="action-content">
-          <a>
+          <NavLink
+            to="/"
+            activeClassName="is-active"
+          >
             offer a <span className="boldItalic">service / collaborator</span>
-          </a>
+          </NavLink>
         </div>
       </div>
     </div>
