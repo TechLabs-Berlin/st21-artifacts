b3a27bd3a7d0f70dcaf884013d794af446468ca4
Steffi <zastrow.stefanie@googlemail.com>
2021-07-07 22:19:14 +0200

    eslint styled code

--- WD/src/scripts/components/ContactForm.js
+++ WD/src/scripts/components/ContactForm.js
@@ -1,35 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import emailjs, { send } from 'emailjs-com';
 import { useUserInformation } from '../context/user-context/UserContext';
 
- export default function ContactForm(props) {
- const { userInformation } = useUserInformation();
- const [message, setMessage] = useState();
+export default function ContactForm(props) {
+  const { userInformation } = useUserInformation();
+  const [ message, setMessage ] = useState();
 
 
-    function sendEmail(e){
-      e.preventDefault();
-    
-      console.log(props.ownerInformation.mail.split(':')[1])
-      emailjs.send('artifactsgmail', 'template_7w88qwe', {
-        from_name: userInformation.name,
-        message_to_send: message,
-        reply_to: userInformation.mail,
-        to_email: props.ownerInformation.mail,
-        to_name: props.ownerInformation.name
-      }, 'user_9AIsWEje6EriowiE0uOR8')
+  function sendEmail(e) {
+    e.preventDefault();
+
+    console.log(props.ownerInformation.mail.split(':')[1]);
+    emailjs.send('artifactsgmail', 'template_7w88qwe', {
+      from_name: userInformation.name,
+      message_to_send: message,
+      reply_to: userInformation.mail,
+      to_email: props.ownerInformation.mail,
+      to_name: props.ownerInformation.name,
+    }, 'user_9AIsWEje6EriowiE0uOR8')
         .then((result) => {
-            console.log(result.text);
+          console.log(result.text);
         }, (error) => {
-            console.log(error.text);
+          console.log(error.text);
         });
-      }
-  
+  }
+
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>  
+    <form className="contact-form" onSubmit={sendEmail}>
       <div>
-        <textarea className="message-to-send" name="message_to_send" placeholder={"Write " + props.ownerInformation.name + " a message..."} value={message} onChange={ (e) => setMessage(e.target.value)}  /> 
+        <textarea className="message-to-send" name="message_to_send" placeholder={'Write ' + props.ownerInformation.name + ' a message...'} value={message} onChange={ (e) => setMessage(e.target.value)} />
       </div>
       <input type="submit" value="Send" className="contact-owner"/>
     </form>
--- WD/src/scripts/components/ItemPopover.js
+++ WD/src/scripts/components/ItemPopover.js
@@ -17,31 +17,33 @@ const ItemPopover = (props) => {
   handleOtherUserInformation(props.ownerKey);
 
   return (
-  <div className="item-popover-background" onClick={props.clearPopover}>
-    <div className="item-popover" onClick={(event) => { event.stopPropagation()}}>
-      <div className="item-popover-left-side">
-        <div className="item-popover-offerer">
-          <div className="item-popover-offerer-pic-container" style={{backgroundImage: 'url(' +otherUserInformation.profilePicture + ')', }} />
-          <p className="item-popover-offerer-name">{otherUserInformation.name}</p>
-          <p className="review-owner">{'★'.repeat(Math.round(parseFloat(props.ownerReview))) +
+    <div className="item-popover-background" onClick={props.clearPopover}>
+      <div className="item-popover" onClick={(event) => {
+        event.stopPropagation();
+      }}>
+        <div className="item-popover-left-side">
+          <div className="item-popover-offerer">
+            <div className="item-popover-offerer-pic-container" style={{ backgroundImage: 'url(' +otherUserInformation.profilePicture + ')' }} />
+            <p className="item-popover-offerer-name">{otherUserInformation.name}</p>
+            <p className="review-owner">{'★'.repeat(Math.round(parseFloat(props.ownerReview))) +
                   '☆'.repeat(5 - Math.round(parseFloat(props.ownerReview)))}</p>
+          </div>
+          <h1>{props.itemName}</h1>
+          <div className="item-popover-image-container" style={{
+            backgroundImage: 'url(' + props.itemPicture + ')',
+          }} />
         </div>
-        <h1>{props.itemName}</h1>
-        <div className="item-popover-image-container" style={{
-          backgroundImage: 'url(' + props.itemPicture + ')',
-        }} />
-      </div>
-      <div className="item-popover-right-side">
-        <h2>About this item</h2>
-        <p>📍 Berlin, {props.ownerLocation}</p>
-        <p><b>Price:</b> €{props.itemPrice}</p>
-        <p> <b>Condition:</b> {props.itemCondition}</p>
-        <p><b>Description:</b></p>
-        <p>{props.itemDescription}</p>
-        <ContactForm ownerInformation={otherUserInformation}/>
+        <div className="item-popover-right-side">
+          <h2>About this item</h2>
+          <p>📍 Berlin, {props.ownerLocation}</p>
+          <p><b>Price:</b> €{props.itemPrice}</p>
+          <p> <b>Condition:</b> {props.itemCondition}</p>
+          <p><b>Description:</b></p>
+          <p>{props.itemDescription}</p>
+          <ContactForm ownerInformation={otherUserInformation}/>
+        </div>
       </div>
     </div>
-  </div>
-)
-}
+  );
+};
 export default ItemPopover;
