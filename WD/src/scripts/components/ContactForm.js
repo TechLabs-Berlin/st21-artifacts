59722a288e4965a98c94eab1e19f06d95c0b40eb
sernawills <luciana.sernawills@student.hpi.uni-potsdam.de>
2021-07-07 22:15:26 +0200

    feedback after sending message

--- WD/src/scripts/components/ContactForm.js
+++ WD/src/scripts/components/ContactForm.js
@@ -3,35 +3,67 @@ import emailjs, { send } from 'emailjs-com';
 import { useUserInformation } from '../context/user-context/UserContext';
 
  export default function ContactForm(props) {
- const { userInformation } = useUserInformation();
- const [message, setMessage] = useState();
+  const { userInformation } = useUserInformation();
+  const [message, setMessage] = useState();
+  const [messageStatus, setMessageStatus] = useState("Not sent");
+  const [buttonEnabled, setButtonEnabled] = useState(false);
 
 
-    function sendEmail(e){
-      e.preventDefault();
+  function sendEmail(e){
+    e.preventDefault();
     
-      console.log(props.ownerInformation.mail.split(':')[1])
-      emailjs.send('artifactsgmail', 'template_7w88qwe', {
+    setButtonEnabled(false);
+    console.log(props.ownerInformation.mail.split(':')[1])
+    emailjs.send('artifactsgmail', 'template_7w88qwe', {
         from_name: userInformation.name,
         message_to_send: message,
         reply_to: userInformation.mail,
         to_email: props.ownerInformation.mail,
         to_name: props.ownerInformation.name
       }, 'user_9AIsWEje6EriowiE0uOR8')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-      }
+      .then((result) => {
+          setMessageStatus("Success")
+          console.log(result.text);
+      }, (error) => {
+          setMessageStatus("Error")
+          console.log(error.text);
+      });
+  }
   
+  switch (messageStatus){
 
-  return (
-    <form className="contact-form" onSubmit={sendEmail}>  
-      <div>
-        <textarea className="message-to-send" name="message_to_send" placeholder={"Write " + props.ownerInformation.name + " a message..."} value={message} onChange={ (e) => setMessage(e.target.value)}  /> 
-      </div>
-      <input type="submit" value="Send" className="contact-owner"/>
-    </form>
-  );
+    case 'Not sent':
+      return (
+        <form className="contact-form" onSubmit={sendEmail}>  
+          <div>
+            <textarea 
+              className="message-to-send" 
+              name="message_to_send" 
+              placeholder={"Write " + props.ownerInformation.name + " a message..."} 
+              value={message} 
+              onChange={ (e) => {
+                setMessage(e.target.value);
+                setButtonEnabled(e.target.value);
+                }
+              }
+            /> 
+          </div>
+          {buttonEnabled
+            ?  <input type="submit" value="Send" className="contact-owner"/>
+            :  <input type="submit" value="Send" className="contact-owner" disabled/>
+          }
+        </form>
+      );
+
+    case 'Success': 
+      return (
+        <p className="message-sent-feedback"><b>Message sent!</b> &#9989;
+        </p>
+      ); 
+
+    default : 
+      return (
+        <p>error</p>
+      );
+  }
 }
